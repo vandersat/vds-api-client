65c047934d6d5ffacc6921d3b905abf7717e53ea
teije01 <tvanderhorst@vandersat.com>
2020-12-07 14:43:56 +0100

    Rearrange init with import after package attributes

--- vds_api_client/__init__.py
+++ vds_api_client/__init__.py
@@ -4,12 +4,11 @@ import logging
 
 __version__ = pkg_resources.get_distribution(__name__).version
 
-from vds_api_client.api_v2 import VdsApiV2
-
-
 AUTH = (None, None)
 ENVIRONMENT = 'maps'
 HEADERS = {}
 LOGGER = logging.getLogger('vds_api')
+
+from vds_api_client.api_v2 import VdsApiV2
 
 # EOF
